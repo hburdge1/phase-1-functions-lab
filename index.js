abf2422cb197904cff99e319bd1ae3b75a2d932e
Hannah Burdge <hburdge1@gmail.com>
2021-12-14 07:57:02 -0800

    commit tree 171011471c3c55ce250bb7d331c588596ba498b6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    return Math.abs(start - 42) ;
+} ;
+
+function distanceFromHqInFeet(start) {
+    return (Math.abs(start - 42) * 264);
+} ;
+
+function distanceTravelledInFeet(start, destination) {
+    return (distanceFromHqInFeet(destination) - (distanceFromHqInFeet(start))) ;
+} ;
+
+function  calculatesFarePrice(start, destination) {
+    let truthers = (distanceFromHqInFeet(destination) - (distanceFromHqInFeet(start)));
+    let resp 
+    if (truthers < 400) {
+            resp = 0 ;
+    } else if ((truthers > 400) && (truthers < 2000)) {
+            resp = ((truthers - 400) * 0.02);
+    } else if ((truthers > 2000) && (truthers < 2500)) {
+            resp = 25 ;
+    } else if (truthers > 2500) {
+            resp = 'cannot travel that far' ;
+    }
+    return resp
+}
